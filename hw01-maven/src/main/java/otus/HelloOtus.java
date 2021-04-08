e7cc2bbb628c3d6a64c132020dd3b3c9f73a5803
Andrey Agalakov <aagalakov@it-platforma.ru>
2021-04-08 12:59:14 +0300

    Объявление переменных final.

--- hw01-maven/src/main/java/otus/HelloOtus.java
+++ hw01-maven/src/main/java/otus/HelloOtus.java
@@ -7,16 +7,16 @@ import java.util.List;
 import java.util.Map;
 
 public class HelloOtus {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         final Map<String, Integer> items = ImmutableMap.of("coin", 3, "glass", 4, "pencil", 1);
 
         items.entrySet()
              .forEach(System.out::println);
 
-        List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi",
+        final List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi",
                 "mandarin", "date", "quince");
 
-        for (String fruit: fruits) {
+        for (final String fruit: fruits) {
             System.out.println(fruit);
         }
     }
