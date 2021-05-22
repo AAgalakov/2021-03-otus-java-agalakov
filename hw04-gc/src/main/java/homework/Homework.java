ca3b0ccb47e5b8374f40df3c95d62ca32636ba5d
Andrey Agalakov <aagalakov@it-platforma.ru>
2021-05-22 10:35:26 +0300

    Правки по четвёртому домашнему заданию.

--- hw04-gc/src/main/java/homework/Homework.java
+++ hw04-gc/src/main/java/homework/Homework.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 public class Homework {
     public static void main(String[] args) {
+        int i = 0;
         List<Integer> integerList = new ArrayList<>();
+        List<Integer> collection = getCollection();
 
         while (true) {
-            integerList.addAll(getCollection());
-            integerList = new ArrayList<>(integerList);
+            i++;
+            integerList.addAll(collection);
+            if (i % 2 == 0) {
+                integerList.remove(0);
+            }
+            System.out.println(integerList.size());
         }
     }
 
