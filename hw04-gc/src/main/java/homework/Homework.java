5263b5494d546f8df578c20630a8026f03428451
Andrey Agalakov <aagalakov@it-platforma.ru>
2021-05-24 23:03:32 +0300

    Исправления согласно полученной обратной связи по выполнению домашнего задания.

--- hw04-gc/src/main/java/homework/Homework.java
+++ hw04-gc/src/main/java/homework/Homework.java
@@ -1,29 +1,17 @@
 package homework;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Homework {
     public static void main(String[] args) {
-        int i = 0;
-        List<Integer> integerList = new ArrayList<>();
-        List<Integer> collection = getCollection();
+        List<Object[]> integerList = new LinkedList<>();
 
-        while (true) {
-            i++;
-            integerList.addAll(collection);
-            if (i % 2 == 0) {
+        for (int j = 1; j > 0; j++) {
+            integerList.add(new Object[100]);
+            if ((j % 2) == 0) {
                 integerList.remove(0);
             }
-            System.out.println(integerList.size());
         }
-    }
-
-    private static List<Integer> getCollection() {
-        ArrayList<Integer> integers = new ArrayList<>();
-        for (int i = 0; i < 10_000; i++) {
-            integers.add(i);
-        }
-        return integers;
     }
 }
