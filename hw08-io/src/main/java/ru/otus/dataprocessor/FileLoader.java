11958fc6d73b3fe460c61394439a5fdad9738ffb
Andrey Agalakov <aagalakov@it-platforma.ru>
2021-07-15 23:40:56 +0300

    Внёс правки по обратной связи после проверки.

--- hw08-io/src/main/java/ru/otus/dataprocessor/FileLoader.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/FileLoader.java
@@ -5,7 +5,6 @@ import ru.otus.model.Measurement;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonValue;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,14 +18,17 @@ public class FileLoader implements Loader {
 
     @Override
     public List<Measurement> load() {
-        List<Measurement> list = new ArrayList<>();
+        List<Measurement> list;
         try (var jsonReader = Json.createReader(Measurement.class.getClassLoader().getResourceAsStream(fineName))) {
             final JsonArray jsonValues = jsonReader.readArray();
-            final List<Measurement> collect = jsonValues.getValuesAs(JsonValue::asJsonObject)
-                    .stream()
-                    .map(jsonObject -> new Measurement(jsonObject.get("name").toString(), Double.parseDouble(jsonObject.get("value").toString())))
-                    .collect(Collectors.toList());
-            list.addAll(collect);
+            list = jsonValues.getValuesAs(JsonValue::asJsonObject)
+                             .stream()
+                             .map(jsonObject ->
+                                     new Measurement(
+                                             jsonObject.get("name").toString().replace("\"", ""),
+                                             Double.parseDouble(jsonObject.get("value").toString())
+                                     ))
+                             .collect(Collectors.toList());
         }
         return list;
     }
